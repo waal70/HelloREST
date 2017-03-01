019e743c2972320398909e775cbd536817165e21
André <waal70@hotmail.com>
2017-03-01 15:19:43 +0100

    Initial commit

---
+++ src/org/waal70/rest/app/HelloWorldApplication.java
@@ -0,0 +1,9 @@
+package org.waal70.rest.app;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("rest")
+public class HelloWorldApplication extends ResourceConfig {}
+
